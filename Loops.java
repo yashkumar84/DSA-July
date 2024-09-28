235297c29d4c52ccf682b10db15369b3d2389acc
yashkumar84 <166642691+yashkumar84@users.noreply.github.com>
2024-09-28 13:38:37 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Loops {
+
+  public static void main(String[] args) {
+    
+    // for(int i=0;i<10;i++){
+    //   if(i == 5 )break;
+    //   System.out.println(i);
+    // }
+    Scanner sc = new Scanner(System.in);
+    // int n = sc.nextInt();
+    // System.out.println(n);
+    // String name = sc.next();
+    // System.out.println(name);
+    // sc.nextLine();
+    // String fullName = sc.nextLine();
+    // System.out.println(fullName);
+
+    float f = 1.0f;
+    System.out.println(f);
+    double d = 2.009;
+    System.out.println(d);
+
+  }
+  
+}
